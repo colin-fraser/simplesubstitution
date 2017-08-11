6c8ebdeac1c50271c3cc9d285b3a6731ea1883ec
Colin Fraser <colin.fraser@telus.com>
2017-08-11 20:13:18 +0000

    updated docs

--- R/simplesubstitution.R
+++ R/simplesubstitution.R
@@ -7,10 +7,28 @@ list_parameters <- function(s) {
   unlist(str_extract_all(s, pp))
 }
 
+#' Substitution
+#'
+#' @param s A string in which to make replacements
+#' @param params A named list of substitution parameters
+#' @param pattern The pattern that encodes parameters.
+#'
+#' @details
+#' Given a string with parameters encoded inside braces, \code{substitution} replaces the parameters with values given in the
+#' `params` argument. This is mostly useful for parameterizing SQL code.
+#'
+#' @return A string with replacements made.
+#' @export
+#'
+#' @examples
+#' substitution("SELECT {thing} FROM {place}", list(thing = "TOOLS", place = "TOOLSHED"))
 substitution <- function(s, params, pattern = pp) {
 
   # Check that params matches the params in s
   s_params <- list_parameters(s)
+  if (length(s_params) < 1) {
+    warning("No parameters detected in string.")
+  }
 
   for (param in s_params) {
     if (!(param %in% names(params))) {
